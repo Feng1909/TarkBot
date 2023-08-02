fc3ae820c329e7c0977b00fdc6f230215c9df15e
yunji.feng <732792434@qq.com>
2023-08-02 23:33:16 +0800

    update

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt install ros-noetic-libuvc-ros
+sudo apt install liborocos-bfl-dev
+sudo apt install ros-noetic-costmap-2d
+sudo apt install ros-noetic-move-base-msgs
+sudo apt install ros-noetic-async-web-server-cpp
+
